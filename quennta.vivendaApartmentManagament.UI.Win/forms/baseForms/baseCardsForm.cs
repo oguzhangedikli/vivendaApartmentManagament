f052fb8350df579d95142d4006ff3b58ddb8740e
Oguzhan Gedikli <oguzhangdkl@gmail.com>
2020-06-14 03:36:17 +0300

    Added BaseForm

---
+++ quennta.vivendaApartmentManagament.UI.Win/forms/baseForms/baseCardsForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace quennta.vivendaApartmentManagament.UI.Win.forms.baseForms
+{
+    public partial class baseCardsForm : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public baseCardsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
