04368e64efbd7a0e6e6706c5292287b611f17336
Oguzhan Gedikli <oguzhangdkl@gmail.com>
2020-06-14 18:47:13 +0300

    Build MyTextEdit Compenent

---
+++ quennta.vivendaApartmentManagament.UI.Win/UserControls/Controls/myTextEdit.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.XtraEditors;
+using quennta.vivendaApartmentManagament.UI.Win.Interfaces;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace quennta.vivendaApartmentManagament.UI.Win.UserControls.Controls
+{
+    public class myTextEdit:TextEdit,IstatusBarAciklama
+    {
+        [ToolboxItem(true)]
+        public myTextEdit()
+        {
+            Properties.AppearanceFocused.BackColor = Color.LightBlue;
+            Properties.MaxLength = 50;
+
+        }
+        public override bool EnterMoveNextControl { get; set; } = true;
+        public string statusBarAciklama { get; set; }
+    }
+}
