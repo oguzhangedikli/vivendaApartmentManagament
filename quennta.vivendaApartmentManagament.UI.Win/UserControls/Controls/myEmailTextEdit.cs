8071c43e6e04804819f4bfdb7d09314c1dc232a6
Oguzhan Gedikli <oguzhangdkl@gmail.com>
2020-06-15 16:31:31 +0300

    E-Mail Compenents add

---
+++ quennta.vivendaApartmentManagament.UI.Win/UserControls/Controls/myEmailTextEdit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.XtraEditors.Mask;
+using DevExpress.Utils;
+using System.ComponentModel;
+
+namespace quennta.vivendaApartmentManagament.UI.Win.UserControls.Controls
+{
+    [ToolboxItem(true)]
+    public class myEmailTextEdit : myTextEdit
+    {
+        public myEmailTextEdit()
+        {
+            Properties.Mask.MaskType = MaskType.RegEx;
+            Properties.Mask.EditMask = @"((([0-9a-zA-Z_%-])+[.])+|([0-9a-zA-Z_%-])+)+@((([0-9a-zA-Z_-])+[.])+|([0-9a-zA-Z_-])+)+";
+            Properties.Mask.AutoComplete = AutoCompleteType.Strong;
+            statusBarAciklama = "E-Mail Adresi Giriniz";
+        }
+    }
+}
