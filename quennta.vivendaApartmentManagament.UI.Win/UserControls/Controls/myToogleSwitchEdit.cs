422e43e07c50db986660e46dc7683512dbbbb68b
Oguzhan Gedikli <oguzhangdkl@gmail.com>
2020-06-15 16:38:17 +0300

    ToogleSwitch Compenent

---
+++ quennta.vivendaApartmentManagament.UI.Win/UserControls/Controls/myToogleSwitchEdit.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.XtraEditors;
+using System.Drawing;
+using DevExpress.Utils;
+using quennta.vivendaApartmentManagament.UI.Win.Interfaces;
+using System.ComponentModel;
+
+namespace quennta.vivendaApartmentManagament.UI.Win.UserControls.Controls
+{
+    public class myToogleSwitchEdit : ToggleSwitch,IstatusBarAciklama
+    {
+        [ToolboxItem(true)]
+        public myToogleSwitchEdit()
+        {
+            Name = "tglDurum";
+            Properties.OffText = "Pasif";
+            Properties.OnText = "Aktif";
+            Properties.AutoHeight = false;
+            Properties.AutoWidth = true;
+            Properties.GlyphAlignment = HorzAlignment.Far;
+            Properties.Appearance.ForeColor = Color.Yellow;
+
+        }
+        public override bool EnterMoveNextControl { get; set; } = true;
+
+        public string statusBarAciklama { get; set; } = "Kartın Kullanım Durumunu Seçiniz.";
+    }
+}
